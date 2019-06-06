fef4b94ac72ab185b6cfc32c137a8dd65bf58926
Melissa Arrington <melissaarrington@Melissas-MacBook-Air.local>
2019-06-06 09:51:04 -0400

    heroku changes

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+// require("dotenv").config();
+// const mongoose = require("mongoose");
 
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect("mongodb://localhost/tea-tree");
-}
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect("mongodb://localhost/tea-tree");
+// }
