f52b8c95bab1523500c35f9b07906c210523a2ab
Melissa Arrington <melissaarrington@Melissas-MacBook-Air.local>
2019-06-06 00:08:34 -0400

    Adding Heroku

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get("/", (req, res) => {
   res.send("Hello Worldddd!");
 });
 
+app.use(express.static(__dirname + "/client/build/"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
