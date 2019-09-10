4d6c7c3406bc39e7f893eec6cc3307af00ad1d12
Melissa Arrington <melissa.arrington1@yahoo.com>
2019-09-10 12:27:06 -0400

    creating new user page in client

---
+++ client/src/Components/CreateUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateUser() {
+  return (
+    <div>
+      Hey From Create User. Here, we will make a form that will ask for name,
+      email, password
+    </div>
+  );
+}
+
+export default CreateUser;
