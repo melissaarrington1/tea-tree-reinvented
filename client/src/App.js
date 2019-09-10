d9ed1c7e7f786e9e2b7cefcdef26ffb23dc86cc2
Melissa Arrington <melissa.arrington1@yahoo.com>
2019-09-10 17:12:39 -0400

    created user form using hooks

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HomePage from "./Components/HomePage";
 import TeaInfoPage from "./Components/TeaInfoPage";
+import CreateUser from "./Components/CreateUser";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={HomePage} />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/create" component={CreateUser} />
     </BrowserRouter>
   );
 }
--- client/src/Components/CreateUser.js
+++ client/src/Components/CreateUser.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateUser() {
+  const [userInput, setUserInput] = useState("");
+
+  const inputChangeHandler = event => {
+    setUserInput(event.target.value);
+  };
   return (
     <div>
       Hey From Create User. Here, we will make a form that will ask for name,
       email, password
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        onChange={inputChangeHandler}
+        value={userInput}
+      />
+      <input
+        type="text"
+        name="email"
+        placeholder="Email"
+        onChange={inputChangeHandler}
+        value={userInput}
+      />
+      <input
+        type="text"
+        name="password"
+        placeholder="Password"
+        onChange={inputChangeHandler}
+        value={userInput}
+      />
+      <button>Submit</button>
     </div>
   );
 }
